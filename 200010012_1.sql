ad5b1b2da626706189faccc0b18387bf98b4f864
Devdatt N <83585711+devblixt@users.noreply.github.com>
2022-09-06 19:07:16 +0530

    question 5

--- 200010012_1.sql
+++ 200010012_1.sql
@@ -34,4 +34,19 @@ INSERT INTO part VALUES (1000,'Steel Bolt','Black',1);
 INSERT INTO supplier VALUES (2000,'Adithya Narayan','Trivandrum','Airport Bank');
 INSERT INTO shipment VALUES(3000,1000,2000,'2022-09-07',100,1);
 
--- Q5) 
+-- Q5) Queries for each question
+
+-- (a) List suppliers who have supplied red parts.
+SELECT DISTINCT `sup-name` FROM (part NATURAL JOIN shipment) NATURAL JOIN supplier WHERE color = "red";
+
+-- (b) Get the total cost of shipments for all suppliers for making payments to them.
+SELECT `supplier-no`, `sup-name`, SUM(quantity*price) FROM shipment NATURAL JOIN supplier GROUP BY shipment.`supplier-no`;
+
+-- (c) List suppliers who have supplied all parts
+SELECT supplier.`supplier-no`, supplier.`sup-name`
+FROM (part NATURAL JOIN shipment) NATURAL JOIN supplier
+GROUP BY supplier.`supplier-no`
+HAVING COUNT(DISTINCT part.`part-no`) = (
+    SELECT COUNT(DISTINCT part.`part-no`)
+    FROM part
+);
