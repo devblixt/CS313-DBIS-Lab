167f44e21a6a3d0996c8026ff5d27fcc4dd95218
Devdatt N <devblixt@gmail.com>
2022-09-06 18:18:08 +0530

    Create 200010012_1.sql
    a bit

---
+++ 200010012_1.sql
@@ -0,0 +1,30 @@
+-- Q1) Creating User and Database named lab3
+CREATE USER 'lab3'@'localhost' IDENTIFIED BY 'wenomechainsama';
+CREATE DATABASE lab3;
+GRANT ALL PRIVILEGES ON lab3.* TO 'lab3'@'localhost';
+
+-- Q2) Creating Tables
+USE lab3;
+CREATE TABLE part(
+    `part-no` INT(6) PRIMARY KEY,
+    `part-name` VARCHAR(45) NOT NULL,
+    color VARCHAR(7),
+    weight NUMERIC(7,3)
+    );
+CREATE TABLE supplier(
+    `supplier-no` INT(5) PRIMARY KEY,
+    `sup-name` VARCHAR(45) NOT NULL,
+    city VARCHAR(30) NOT NULL, 
+    bank VARCHAR(30)
+    );
+CREATE TABLE shipment(
+    `shipment-no` INT(7) PRIMARY KEY,
+    `part-no` INT(6) NOT NULL,
+    `supplier-no` INT(5) NOT NULL,
+    date DATE,
+    quantity INT(5),
+    price NUMERIC(7,2),
+    FOREIGN KEY (`part-no`) REFERENCES part(`part-no`),
+    FOREIGN KEY (`supplier-no`) REFERENCES supplier(`supplier-no`)
+    );
+
